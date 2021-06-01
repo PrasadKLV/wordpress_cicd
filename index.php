056e889b69c374b7274a79b17a6cacea676e4c11
prasadklv <prasad.klv@cloud4c.com>
2021-06-01 18:06:34 +0530

    june01-0559

--- index.php
+++ index.php
@@ -15,5 +15,8 @@ define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
+<<<<<<< HEAD
 
 Naresh
+=======
+>>>>>>> parent of 9f504b8... june01-0539
